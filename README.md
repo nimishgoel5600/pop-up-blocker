bbde32789d05f182a5f9887206ccfd1eac643367
george <georgios.perivolarakis@gamelounge.com>
2016-12-21 20:55:24 +0200

    Initial commit. Working code for specific CSS class names.

---
+++ README.md
@@ -0,0 +1,1 @@
+# pop-up-blocker
---
+++ content.js
@@ -0,0 +1,92 @@
+"use strict";
+
+class Modal {
+
+    constructor(name, shadow) {
+        this.name = name;
+        this.shadow = shadow
+    }
+
+    getModals() {
+        return document.getElementsByClassName(this.name);
+    }
+
+    getBackdrops() {
+        return document.getElementsByClassName(this.shadow);
+    }
+
+    getAll() {
+        let all = [];
+        all.push(this.getModals(), this.getBackdrops());
+
+        return all;
+    }
+
+    deleteAll() {
+        let els = this.getAll();
+        let size = els.length;
+        let temp = [];
+
+        if (size > 0) {
+
+            els.forEach(function sk(item) { // could be 1 DOM element or HTMLCollection
+
+                if (item.length > 0) {
+                    for (let j = 0; j < item.length; j++) {
+                        temp.push(item[j]);
+                    }
+                }
+                else {
+                    temp.push(item);
+                }
+            });
+
+        }
+
+        temp.forEach(function d(el){
+            if (el.parentNode) {
+                el.parentNode.removeChild(el);
+            }
+        });
+
+        this.setScroll(); //
+
+        return temp; // all elements
+    }
+
+    setScroll() {
+        document.getElementsByTagName('body')[0].style.overflow = 'scroll';
+    }
+
+}
+
+// Init App
+setTimeout(function () {
+
+    let modal = new Modal('tp-modal', 'tp-backdrop');
+    let counter = 0;
+
+    let stopInterval = function () {
+        window.clearInterval(intervalID);
+    };
+
+    let intervalID = window.setInterval(function () {
+
+        console.log(counter);
+        counter++;
+
+        if (counter === 50) {
+            stopInterval();
+        }
+
+        if (document.getElementsByClassName('tp-modal').length > 0) {
+            modal.deleteAll();
+        }
+        // else {
+        //     stopInterval();
+        // }
+    }, 200);
+
+}, 3000);
+
+
